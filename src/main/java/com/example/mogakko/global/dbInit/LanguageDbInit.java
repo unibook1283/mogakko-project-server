58f0b44d9dc6f5d5eda5f7f18aaa98741dbf14a3
unibook1283 <uni_book@naver.com>
2023-01-01 20:00:25 +0900

    feat: initialize language db

---
+++ src/main/java/com/example/mogakko/global/dbInit/LanguageDbInit.java
@@ -0,0 +1,69 @@
+package com.example.mogakko.global.dbInit;
+
+import com.example.mogakko.domain.values.dto.LanguageDTO;
+import com.example.mogakko.domain.values.service.LanguageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class LanguageDbInit {
+
+    private final LanguageService languageService;
+
+    @PostConstruct
+    private void insertLanguagesIntoDb() {
+        List<String> languages = List.of(
+                // 프론트엔드
+                "JavaScript",
+                "TypeScript",
+                "React",
+                "Vue",
+                "Svelte",
+                "Angular",
+                // 백엔드
+                "Spring",
+                "Laravel",
+                "NodeJS",
+                "NestJS",
+                "Go",
+                "Kotlin",
+                "Express",
+                "Django",
+                "php",
+                "Flask",
+                "Ruby on Rails",
+                // db
+                "GraphQL",
+                "MySQL",
+                "Oracle",
+                "MongoDB",
+                "PostgreSQL",
+                "MariaDB",
+                // 모바일
+                "Flutter",
+                "Swift",
+                "ReactNative",
+                "Unity",
+                // 기타
+                "aws",
+                "Kubernetes",
+                "Docker",
+                "Git",
+                "Figma",
+                "Zeplin",
+                "Jest",
+                "Firebase"
+        );
+
+        languages.stream()
+                .forEach(language -> {
+                    LanguageDTO languageDTO = new LanguageDTO();
+                    languageDTO.setName(language);
+                    languageService.saveLanguage(languageDTO);
+                });
+    }
+}
